e64f749415d599557b6995b14a4690fabe2df807
a.guzev <aguzev@sfxdx.ru>
2020-04-17 23:51:48 +1000

    go test -run=TestFlushableParallelXXX -count=1 ./kvdb/flushable

--- kvdb/flushable/flushable_parallel_test.go
+++ kvdb/flushable/flushable_parallel_test.go
@@ -15,6 +15,55 @@ import (
 	"github.com/Fantom-foundation/go-lachesis/kvdb/table"
 )
 
+func TestFlushableParallelXXX(t *testing.T) {
+	testPairsNum := uint64(1000)
+
+	dir, err := ioutil.TempDir("", "test-flushable")
+	if err != nil {
+		panic(fmt.Sprintf("can't create temporary directory %s: %v", dir, err))
+	}
+	disk := leveldb.NewProducer(dir)
+
+	// open raw databases
+	ldb := disk.OpenDb("1")
+	defer ldb.Drop()
+	defer ldb.Close()
+
+	flushableDb := Wrap(ldb)
+
+	tableMutable1 := table.New(flushableDb, []byte("1"))
+	tableImmutable := table.New(flushableDb, []byte("2"))
+	tableMutable2 := table.New(flushableDb, []byte("3"))
+
+	// fill data
+	for i := uint64(0); i < testPairsNum; i++ {
+		_ = tableImmutable.Put(bigendian.Int64ToBytes(i), bigendian.Int64ToBytes(i))
+		if i == testPairsNum/2 { // a half of data is flushed, other half isn't
+			_ = flushableDb.Flush()
+		}
+	}
+
+	r := rand.New(rand.NewSource(0))
+	assertar := assert.New(t)
+
+	// iterate over tableImmutable and check its content
+	it := tableImmutable.NewIterator()
+	defer it.Release()
+	i := uint64(0)
+	for ; it.Next(); i++ {
+		assertar.Equal(bigendian.Int64ToBytes(i), it.Key(), i)
+		assertar.Equal(bigendian.Int64ToBytes(i), it.Value(), i)
+		assertar.NoError(it.Error(), i)
+
+		_ = tableMutable1.Put(bigendian.Int64ToBytes(r.Uint64()%testPairsNum), bigendian.Int64ToBytes(r.Uint64()))
+		_ = tableMutable2.Put(bigendian.Int64ToBytes(r.Uint64() % testPairsNum)[:7], bigendian.Int64ToBytes(r.Uint64()))
+		// if i == 0x15f-1 {
+		_ = flushableDb.Flush()
+		//}
+	}
+	assertar.Equal(testPairsNum, i)
+}
+
 func TestFlushableParallel(t *testing.T) {
 	testDuration := 2 * time.Second
 	testPairsNum := uint64(1000)
@@ -44,44 +93,54 @@ func TestFlushableParallel(t *testing.T) {
 		}
 	}
 
-	stopped := false
-
+	done := make(chan struct{})
 	wg := sync.WaitGroup{}
 	wg.Add(2)
+
 	go func() {
+		defer wg.Done()
 		assertar := assert.New(t)
-		for !stopped {
+		cicle := 0
+		for {
+			select {
+			case <-done:
+				return
+			default:
+			}
 			// iterate over tableImmutable and check its content
 			it := tableImmutable.NewIterator()
-			defer it.Release()
+			// defer it.Release()
 			i := uint64(0)
 			for ; it.Next(); i++ {
 				assertar.Equal(bigendian.Int64ToBytes(i), it.Key(), i)
 				assertar.Equal(bigendian.Int64ToBytes(i), it.Value(), i)
-
 				assertar.NoError(it.Error(), i)
 			}
-			assertar.Equal(testPairsNum, i)
+			it.Release()
+			assertar.Equalf(testPairsNum, i, "cicle %d", cicle)
+			cicle++
 		}
-
-		wg.Done()
 	}()
 
 	go func() {
+		defer wg.Done()
 		r := rand.New(rand.NewSource(0))
-		for !stopped {
+		for {
+			select {
+			case <-done:
+				return
+			default:
+			}
 			// try to spoil data in tableImmutable by updating other tables
 			_ = tableMutable1.Put(bigendian.Int64ToBytes(r.Uint64()%testPairsNum), bigendian.Int64ToBytes(r.Uint64()))
 			_ = tableMutable2.Put(bigendian.Int64ToBytes(r.Uint64() % testPairsNum)[:7], bigendian.Int64ToBytes(r.Uint64()))
-			if r.Int63n(100) == 0 {
+			if r.Int63n(10) == 0 {
 				_ = flushableDb.Flush() // flush with 1% chance
 			}
 		}
-
-		wg.Done()
 	}()
 
 	time.Sleep(testDuration)
-	stopped = true
+	close(done)
 	wg.Wait()
 }
